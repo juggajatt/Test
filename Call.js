eb68cb002eb3c053f4843c3e2b9b9fcc2a4deea7
juggajatt <64840541+juggajatt@users.noreply.github.com>
2020-05-06 01:52:33 +0530

    Update Call.js

--- Call.js
+++ Call.js
@@ -1,9 +1,5 @@
-function get_json(Test.json) {
-	var requester = create_http_request();
-	requester.open("GET", Test.json, false);
-	
-	requester.send(null);
-	var json_text = requester.responseText;
-	var data = json_parser(json_text);
-	return data;
-}
+let github_link = ‘https://juggajatt.github.io/Test/Test.json';
+$.getJSON(github_link, function(data) {
+     //data is the JSON string
+     console.log(data);
+});
