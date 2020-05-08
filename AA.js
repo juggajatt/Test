150926d5039b0d03795731c05ac5de3075c1caf1
juggajatt <64840541+juggajatt@users.noreply.github.com>
2020-05-08 15:48:49 +0530

    Update AA.js

--- AA.js
+++ AA.js
@@ -1,4 +1,17 @@
 <script> 
-// using document.write 
-document.write('Hello World'); 
+// Requiring fs module in which 
+// readFile function is defined. 
+const fs = require('fs') 
+  
+// Reading data in utf-8 format 
+// which is a type of character set. 
+// Instead of 'utf-8' it can be  
+// other character set also like 'ascii' 
+fs.readFile('Test.json', 'utf-8', (err, data) => { 
+    if (err) throw err; 
+  
+    // Converting Raw Buffer to text 
+    // data using tostring function. 
+    console.log(data); 
+}) 
 </script> 
