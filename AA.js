48a9b2bf618585ad68292b9744ccf5edafa3cd60
juggajatt <64840541+juggajatt@users.noreply.github.com>
2020-05-08 15:53:56 +0530

    Update AA.js

--- AA.js
+++ AA.js
@@ -1,7 +1,11 @@
-<script> 
-const fs = require('fs') 
-fs.readFile('Test.json', 'utf-8', (err, data) => { 
-    if (err) throw err; 
-    console.log(data); 
-}) 
-</script> 
+<html>
+    <body>
+        <script> 
+        const fs = require('fs') 
+        fs.readFile('Test.json', 'utf-8', (err, data) => { 
+            if (err) throw err; 
+            console.log(data); 
+        }) 
+        </script> 
+    </body>
+    </html>
